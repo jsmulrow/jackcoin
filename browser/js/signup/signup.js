856f629f7218d2c1094365e6b5d01db1b533541e
Jack Mulrow <jack.mulrow@gmail.com>
2015-12-17 21:23:47 -0500

    moved signup folder

---
+++ browser/js/signup/signup.js
@@ -0,0 +1,29 @@
+app.config(function ($stateProvider) {
+
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: 'js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
+
+});
+
+app.controller('SignupCtrl', function ($scope, AuthService, $state) {
+
+    $scope.signup = {};
+    $scope.error = null;
+
+    $scope.sendLogin = function (signupInfo) {
+
+        $scope.error = null;
+
+        AuthService.signup(signupInfo).then(function (user) {
+            console.log(user);
+            $state.go('chain');
+        }).catch(function () {
+            $scope.error = 'Invalid signup credentials.';
+        });
+
+    };
+
+});
